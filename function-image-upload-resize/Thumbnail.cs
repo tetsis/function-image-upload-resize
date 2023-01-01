e6b93bce4af50f2a86d615b0aee36a1d224d8c69
tetsis <hitoshi.zin.nakao@gmail.com>
2023-01-01 21:34:04 +0900

    プロジェクト ファイルを追加します。

---
+++ function-image-upload-resize/Thumbnail.cs
@@ -0,0 +1,111 @@
+﻿using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventGrid.SystemEvents;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace function_image_upload_resize
+{
+    public static class Thumbnail
+    {
+        private static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+        private static string GetBlobNameFromUrl(string bloblUrl)
+        {
+            var uri = new Uri(bloblUrl);
+            var blobClient = new BlobClient(uri);
+            return blobClient.Name;
+        }
+
+        private static IImageEncoder GetEncoder(string extension)
+        {
+            IImageEncoder encoder = null;
+
+            extension = extension.Replace(".", "");
+
+            var isSupported = Regex.IsMatch(extension, "gif|png|jpe?g", RegexOptions.IgnoreCase);
+
+            if (isSupported)
+            {
+                switch (extension.ToLower())
+                {
+                    case "png":
+                        encoder = new PngEncoder();
+                        break;
+                    case "jpg":
+                        encoder = new JpegEncoder();
+                        break;
+                    case "jpeg":
+                        encoder = new JpegEncoder();
+                        break;
+                    case "gif":
+                        encoder = new GifEncoder();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return encoder;
+        }
+
+        [FunctionName("Thumbnail")]
+        public static async Task Run(
+            [EventGridTrigger]EventGridEvent eventGridEvent,
+            [Blob("{data.url}", FileAccess.Read)] Stream input,
+            ILogger log)
+        {
+            try
+            {
+                if (input != null)
+                {
+                    var createdEvent = JsonSerializer.Deserialize<StorageBlobCreatedEventData>(eventGridEvent.Data.ToString());
+                    var extension = Path.GetExtension(createdEvent.Url);
+                    var encoder = GetEncoder(extension);
+
+                    if (encoder != null)
+                    {
+                        var thumbnailWidth = Convert.ToInt32(Environment.GetEnvironmentVariable("THUMBNAIL_WIDTH"));
+                        var thumbContainerName = Environment.GetEnvironmentVariable("THUMBNAIL_CONTAINER_NAME");
+                        var blobServiceClient = new BlobServiceClient(BLOB_STORAGE_CONNECTION_STRING);
+                        var blobContainerClient = blobServiceClient.GetBlobContainerClient(thumbContainerName);
+                        var blobName = GetBlobNameFromUrl(createdEvent.Url);
+
+                        using (var output = new MemoryStream())
+                        using (var image = Image.Load(input))
+                        {
+                            var divisor = image.Width / thumbnailWidth;
+                            var height = Convert.ToInt32(Math.Round((decimal)(image.Height / divisor)));
+
+                            image.Mutate(x => x.Resize(thumbnailWidth, height));
+                            image.Save(output, encoder);
+                            output.Position = 0;
+                            await blobContainerClient.UploadBlobAsync(blobName, output);
+                        }
+                    }
+                    else
+                    {
+                        log.LogInformation($"No encoder support for: {createdEvent.Url}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation(ex.Message);
+                throw;
+            }
+        }
+    }
+}
